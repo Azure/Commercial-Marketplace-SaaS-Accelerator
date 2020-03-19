450a7230c72bc8738ef833a304a23131036c9a62
Prasad Yalla <prasad.yalla@spektrasystems.com>
2020-03-19 10:23:43 +0530

    Active fetching from application config

---
+++ src/Microsoft.Marketplace.SaaS.SDK.Client/Models/EmailTriggerStatusEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.Marketplace.SaasKit.Models
+{
+    /// <summary>
+    /// Sets Subscription Operation Status
+    /// </summary>
+    public enum EmailTriggerStatusEnum
+    {
+        /// <summary>
+        /// Is Active Enable
+        /// </summary>
+        IsActiveEmailEnabled,
+
+        /// <summary>
+        ///Is Unsubscribe Email  Enable
+        /// </summary>
+        IsUnsubscribeEmailEnabled
+      
+    }
+}
