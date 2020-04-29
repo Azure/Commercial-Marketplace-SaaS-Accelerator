00220a87eb3a43e37fb5279b32a1575cbc170204
Kiran Banda <kiran.banda@spektrasystems.com>
2020-04-29 13:01:11 +0530

    .NET Core 3.1 based webjob with DI for services.

---
+++ src/SaaS.SDK.Provisioning.Webjob/Functions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Marketplace.SaasKit.Client.DataAccess.Contracts;
+using Microsoft.Marketplace.SaasKit.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaaS.SDK.Provisioning.Webjob
+{
+    public class Functions
+    {
+        protected readonly IFulfillmentApiClient fulfillmentApiClient;
+        protected readonly ISubscriptionsRepository subscriptionRepository;
+
+        public Functions(IFulfillmentApiClient fulfillmentApiClient,
+                            ISubscriptionsRepository subscriptionRepository)
+        {
+            this.fulfillmentApiClient = fulfillmentApiClient;
+            this.subscriptionRepository = subscriptionRepository;
+        }
+
+        public void ProcessQueueMessage([QueueTrigger("saas-provisioning-queue")] string message,
+                                                                                           Microsoft.Extensions.Logging.ILogger logger)
+        {
+            logger.LogInformation($"{message} and FulfillmentClient is null : ${fulfillmentApiClient == null}");
+
+            // Deserialize the message as object
+            // Do process
+        }
+    }
+}
