33be533edbcf9fb82f873c6641d699b3ff8a3344
Kiran Banda <kiran.banda@spektrasystems.com>
2020-06-20 19:42:36 +0530

    Removed binaries from the repo. Updated the Powershell script to prepare publish files for the web applications.

--- deployment/Templates/Deploy.ps1
+++ deployment/Templates/Deploy.ps1
@@ -3,20 +3,19 @@
 #
 
 Param(  
-[string]$WebAppNamePrefix,              # Prefix used for creating web applications
-[string]$TenantID,                      # The value should match the value provided for Active Directory TenantID in the Technical Configuration of the Transactable Offer in Partner Center
-[string]$ADApplicationID,               # The value should match the value provided for Active Directory Application ID in the Technical Configuration of the Transactable Offer in Partner Center
-[string]$ADApplicationSecret,           # Secret key of the AD Application
-[string]$SQLServerName,                 # Name of the database server (without database.windows.net)
-[string]$SQLAdminLogin,                 # SQL Admin login
-[string]$SQLAdminLoginPassword,         # SQL Admin password
-[string]$PublisherAdminUsers,           # Provide a list of email addresses (as comma-separated-values) that should be granted access to the Publisher Portal
-[string]$PathToWebApplicationPackages,  # The base URI where artifacts required by this template are located
-[string]$BacpacUrl,                     # The url to the blob storage where the SaaS DB bacpac is stored
-[string]$ResourceGroupForDeployment,    # Name of the resource group to deploy the resources
-[string]$Location,                      # Location of the resource group
-[string]$AzureSubscriptionID,           # Subscription where the resources be deployed
-[string]$PathToARMTemplate              # Local Path to the ARM Template
+   [string]$WebAppNamePrefix, # Prefix used for creating web applications
+   [string]$TenantID, # The value should match the value provided for Active Directory TenantID in the Technical Configuration of the Transactable Offer in Partner Center
+   [string]$ADApplicationID, # The value should match the value provided for Active Directory Application ID in the Technical Configuration of the Transactable Offer in Partner Center
+   [string]$ADApplicationSecret, # Secret key of the AD Application
+   [string]$SQLServerName, # Name of the database server (without database.windows.net)
+   [string]$SQLAdminLogin, # SQL Admin login
+   [string]$SQLAdminLoginPassword, # SQL Admin password
+   [string]$PublisherAdminUsers, # Provide a list of email addresses (as comma-separated-values) that should be granted access to the Publisher Portal
+   [string]$BacpacUrl, # The url to the blob storage where the SaaS DB bacpac is stored
+   [string]$ResourceGroupForDeployment, # Name of the resource group to deploy the resources
+   [string]$Location, # Location of the resource group
+   [string]$AzureSubscriptionID, # Subscription where the resources be deployed
+   [string]$PathToARMTemplate              # Local Path to the ARM Template
 )
 
 #   Make sure to install Az Module before running this script
@@ -28,7 +27,7 @@ $BacpacFileName = "AMPSaaSDB.bacpac"
 $LocalPathToBacpacFile = $TempFolderToStoreBacpac + "\" + $BacpacFileName  
 
 # Create a temporary folder
-New-Item -Path $TempFolderToStoreBacpac -ItemType Directory
+New-Item -Path $TempFolderToStoreBacpac -ItemType Directory -Force
 
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile($BacpacUrl, $LocalPathToBacpacFile)
@@ -38,7 +37,7 @@ $storagepostfix = Get-Random -Minimum 1 -Maximum 1000
 
 $StorageAccountName = "amptmpstorage" + $storagepostfix       #enter storage account name
 
-$ContainerName = "database" #container name for uploading SQL DB file 
+$ContainerName = "packagefiles" #container name for uploading SQL DB file 
 $resourceGroupForStorageAccount = "amptmpstorage"   #resource group name for the storage account.
                                                       
 
@@ -57,22 +56,42 @@ $ctx = New-AzstorageContext -StorageAccountName $StorageAccountName  -StorageAcc
 New-AzStorageContainer -Name $ContainerName -Context $ctx -Permission Blob 
 Set-AzStorageBlobContent -File $LocalPathToBacpacFile -Container $ContainerName -Blob $BlobName -Context $ctx -Force
 
-$URLToBacpacFromStorage =   (Get-AzStorageBlob -blob $BlobName -Container $ContainerName -Context $ctx).ICloudBlob.uri.AbsoluteUri
+$URLToBacpacFromStorage = (Get-AzStorageBlob -blob $BlobName -Container $ContainerName -Context $ctx).ICloudBlob.uri.AbsoluteUri
 
 Write-host "Uploaded the bacpac file to $URLToBacpacFromStorage"    
 
+
+Write-host "Prepare publish files for the web application"
+
+Write-host "Preparing the publish files for PublisherPortal"  
+dotnet publish ..\..\src\SaaS.SDK.PublisherSolution\SaaS.SDK.PublisherSolution.csproj -c debug -o ..\..\Publish\PublisherPortal
+Compress-Archive -Path ..\..\Publish\PublisherPortal\* -DestinationPath ..\..\Publish\PublisherPortal.zip -Force
+
+Write-host "Preparing the publish files for CustomerPortal"
+dotnet publish ..\..\src\SaaS.SDK.CustomerProvisioning\SaaS.SDK.CustomerProvisioning.csproj -c debug -o ..\..\Publish\CustomerPortal
+Compress-Archive -Path ..\..\Publish\CustomerPortal\* -DestinationPath ..\..\Publish\CustomerPortal.zip -Force
+
+Write-host "Upload published files to storage account"
+Set-AzStorageBlobContent -File "..\..\Publish\PublisherPortal.zip" -Container $ContainerName -Blob "PublisherPortal.zip" -Context $ctx -Force
+Set-AzStorageBlobContent -File "..\..\Publish\CustomerPortal.zip" -Container $ContainerName -Blob "CustomerPortal.zip" -Context $ctx -Force
+
+# The base URI where artifacts required by this template are located
+$PathToWebApplicationPackages = ((Get-AzStorageContainer -Container $ContainerName -Context $ctx).CloudBlobContainer.uri.AbsoluteUri)
+
+Write-host "Path to web application packages $PathToWebApplicationPackages"
+
 #Parameter for ARM template, Make sure to add values for parameters before running the script.
 $ARMTemplateParams = @{
-   webAppNamePrefix = "$WebAppNamePrefix"
-   TenantID = "$TenantID"
-   ADApplicationID = "$ADApplicationID"
-   ADApplicationSecret = "$ADApplicationSecret"
-   SQLServerName = "$SQLServerName"
-   SQLAdminLogin = "$SQLAdminLogin"
-   SQLAdminLoginPassword = "$SQLAdminLoginPassword"
-   bacpacUrl = "$URLToBacpacFromStorage"
-   SAASKeyForbacpac = ""
-   PublisherAdminUsers = "$PublisherAdminUsers"
+   webAppNamePrefix             = "$WebAppNamePrefix"
+   TenantID                     = "$TenantID"
+   ADApplicationID              = "$ADApplicationID"
+   ADApplicationSecret          = "$ADApplicationSecret"
+   SQLServerName                = "$SQLServerName"
+   SQLAdminLogin                = "$SQLAdminLogin"
+   SQLAdminLoginPassword        = "$SQLAdminLoginPassword"
+   bacpacUrl                    = "$URLToBacpacFromStorage"
+   SAASKeyForbacpac             = ""
+   PublisherAdminUsers          = "$PublisherAdminUsers"
    PathToWebApplicationPackages = "$PathToWebApplicationPackages"
 }
 
@@ -89,5 +108,6 @@ Write-host "Cleaning things up!"
 # Cleanup : Delete the temporary storage account and the resource group created to host the bacpac file.
 Remove-AzResourceGroup -Name $resourceGroupForStorageAccount -Force 
 Remove-Item –path $TempFolderToStoreBacpac –recurse
+Remove-Item -path "..\..\Publish" -recurse
 
 Write-host "Done!"
