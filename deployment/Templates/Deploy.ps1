df869f684d805faae6bd196c3d4d44eb0e1a61ff
code4clouds <julio.colon@microsoft.com>
2021-09-13 12:42:06 -0400

    removed comma

--- deployment/Templates/Deploy.ps1
+++ deployment/Templates/Deploy.ps1
@@ -16,7 +16,7 @@ Param(
    [string][Parameter(Mandatory)]$BacpacUrl, # The url to the blob storage where the SaaS DB bacpac is stored
    [string][Parameter(Mandatory)]$ResourceGroupForDeployment, # Name of the resource group to deploy the resources
    [string][Parameter(Mandatory)]$Location, # Location of the resource group
-   [string][Parameter(Mandatory)]$PathToARMTemplate,  # Local Path to the ARM Template
+   [string][Parameter(Mandatory)]$PathToARMTemplate  # Local Path to the ARM Template
 )
 
 Write-Host "Starting SaaS Accelerator Deployment..."
