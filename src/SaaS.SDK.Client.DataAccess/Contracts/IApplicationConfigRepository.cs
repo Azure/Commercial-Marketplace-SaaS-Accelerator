f887bdb4069c7c09b3f4e16bdd1873622c4362fa
Santhosh Bomma <sbomma@microsoft.com>
2021-11-04 11:05:05 -0700

    added changes to add admin page for managing app config

--- src/SaaS.SDK.Client.DataAccess/Contracts/IApplicationConfigRepository.cs
+++ src/SaaS.SDK.Client.DataAccess/Contracts/IApplicationConfigRepository.cs
@@ -16,9 +16,22 @@
         string GetValueByName(string name);
 
         /// <summary>
+        /// Gets the value from application configuration.
+        /// </summary>
+        /// <param Id="Id">The App Config Id.</param>
+        /// <returns>value corresponding to the application configuration key.</returns>
+        ApplicationConfiguration GetById(int Id);
+
+        /// <summary>
         /// Gets all the  values from application configuration.
         /// </summary>
         /// <returns>List of key value pairs stored in application configuration.</returns>
         IEnumerable<ApplicationConfiguration> GetAll();
+
+        /// <summary>
+        /// Update an application configuration value.
+        /// </summary>
+        /// <returns>Success or Failure.</returns>
+        int SaveById(ApplicationConfiguration applicationConfiguration);
     }
 }
