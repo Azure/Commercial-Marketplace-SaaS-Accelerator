842e4798c6f1cf564c50b05f186ee48ce1db74d1
Alex Drenea <alexdrenea@gmail.com>
2022-01-16 14:23:44 -0500

    Fx Webhook Controller (fuix #179)
    - Controller POST action should be Task
    - Remove Dispose from ApplicationLogRepository -> shold not need to dispose manually.

--- src/SaaS.SDK.Client.DataAccess/Services/ApplicationLogRepository.cs
+++ src/SaaS.SDK.Client.DataAccess/Services/ApplicationLogRepository.cs
@@ -18,27 +18,12 @@
         private readonly SaasKitContext context;
 
         /// <summary>
-        /// The disposed.
-        /// </summary>
-        private bool disposed = false;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="ApplicationLogRepository"/> class.
         /// </summary>
         /// <param name="context">The this.context.</param>
         public ApplicationLogRepository(SaasKitContext context)
         {
             this.context = context;
-        }
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            this.Dispose(true);
-
-            GC.SuppressFinalize(this);
         }
 
         /// <summary>
@@ -51,21 +36,5 @@
             return this.context.SaveChangesAsync();
         }
 
-        /// <summary>
-        /// Releases unmanaged and - optionally - managed resources.
-        /// </summary>
-        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-                    this.context.Dispose();
-                }
-            }
-
-            this.disposed = true;
-        }
     }
 }
