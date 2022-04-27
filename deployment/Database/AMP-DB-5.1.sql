d69b817b1af0e5daf46b2bffdc839760a1a1d47b
MAGDY SALEM <63874487+msalemcode@users.noreply.github.com>
2022-04-26 18:42:11 -0700

    update schema

--- deployment/Database/AMP-DB-5.1.sql
+++ deployment/Database/AMP-DB-5.1.sql
@@ -435,8 +435,13 @@ GO
 
 CREATE TABLE [dbo].[SchedulerFrequency] (
     [Id]        INT          IDENTITY (1, 1) NOT NULL,
-    [Frequency] VARCHAR (10) NOT NULL
-);
+    [Frequency] VARCHAR (10) NOT NULL,
+CONSTRAINT [PK_SchedulerFrequency] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
 
 
