afa128afc9bd01957304d917158adaa0b8c4c926
David Starr <david@elegantcode.com>
2022-05-13 09:05:34 -0700

    adding test project

---
+++ src/SaaS.SDK.CustomerProvisioning.Test/WebhookControllerTest.cs
@@ -0,0 +1,10 @@
+namespace SaaS.SDK.CustomerProvisioning.Test;
+
+    [TestClass]
+    public class WebhookControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
