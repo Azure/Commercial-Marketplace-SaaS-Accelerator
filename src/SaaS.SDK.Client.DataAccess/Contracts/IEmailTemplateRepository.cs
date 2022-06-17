b69557a87849870eef3ea4ef0a31bb9f92d72ad4
Matthew Grant <t-mgrant@microsoft.com>
2022-06-17 11:15:59 -0700

    reverted EmailTemplate dependency

--- src/SaaS.SDK.Client.DataAccess/Contracts/IEmailTemplateRepository.cs
+++ src/SaaS.SDK.Client.DataAccess/Contracts/IEmailTemplateRepository.cs
@@ -1,7 +1,6 @@
 ﻿namespace Microsoft.Marketplace.SaasKit.Client.DataAccess.Contracts
 {
     using System;
-    using System.Collections.Generic;
     using Microsoft.Marketplace.SaasKit.Client.DataAccess.Entities;
 
     /// <summary>
