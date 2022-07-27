520dee5bc2423eb6759e971212d52b95ed0f58e4
ms-mgrant <t-mgrant@microsoft.com>
2022-07-27 10:45:13 -0700

    modified update function and sub prep function

--- src/SaaS.SDK.Services/Services/SubscriptionService.cs
+++ src/SaaS.SDK.Services/Services/SubscriptionService.cs
@@ -48,12 +48,12 @@ namespace Microsoft.Marketplace.SaaS.SDK.Services.Services
         /// </summary>
         /// <param name="subscriptionDetail">The subscription detail.</param>
         /// <returns>Subscription Id.</returns>
-        public int AddOrUpdatePartnerSubscriptions(SubscriptionResult subscriptionDetail)
+        public int AddOrUpdatePartnerSubscriptions(SubscriptionResult subscriptionDetail, int customerUserId = 0, int subscribeId = 0)
         {
             var isActive = this.IsSubscriptionDeleted(Convert.ToString(subscriptionDetail.SaasSubscriptionStatus));
             Subscriptions newSubscription = new Subscriptions()
             {
-                Id = 0,
+                Id = subscribeId,
                 AmpplanId = subscriptionDetail.PlanId,
                 Ampquantity = subscriptionDetail.Quantity,
                 AmpsubscriptionId = subscriptionDetail.Id,
@@ -63,9 +63,10 @@ namespace Microsoft.Marketplace.SaaS.SDK.Services.Services
                 ModifyDate = DateTime.Now,
                 Name = subscriptionDetail.Name,
                 SubscriptionStatus = Convert.ToString(subscriptionDetail.SaasSubscriptionStatus),
-                UserId = this.currentUserId,
+                UserId = customerUserId == 0 ? this.currentUserId : customerUserId,
                 PurchaserEmail = subscriptionDetail.Purchaser.EmailId,
                 PurchaserTenantId = subscriptionDetail.Purchaser.TenantId,
+                // PlanGUId = subscriptionDetail.PlanGUId // Requires PR#270
             };
             return this.subscriptionRepository.Save(newSubscription);
         }
@@ -156,6 +157,7 @@ namespace Microsoft.Marketplace.SaaS.SDK.Services.Services
                 CustomerEmailAddress = subscription.User?.EmailAddress,
                 CustomerName = subscription.User?.FullName,
                 IsMeteringSupported = existingPlanDetail != null ? (existingPlanDetail.IsmeteringSupported ?? false) : false,
+                // PlanGUId = subscription.PlanGUId // Requires PR#270
             };
             subscritpionDetail.Purchaser = new PurchaserResult();
 
