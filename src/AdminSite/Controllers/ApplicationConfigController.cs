81f792661662982862136a45d8601c4d2bb6fd63
Alex Drenea <alexdrenea@microsoft.com>
2022-12-20 21:06:58 -0500

    Refactor ApplicationConfigController (1)
    - introduce Error middleware
    - Cannot apply at top level of controller due to mix of controller methods (some returning a View, some PartialView and some doing redirects). Errors are handled in the cshtml for those.
    - fixes a bug where the try/catch in methods returning PartialView (ApplicationConfigDetails, EmailTemplateDetails) displays error page in modal (looks bad).

--- src/AdminSite/Controllers/ApplicationConfigController.cs
+++ src/AdminSite/Controllers/ApplicationConfigController.cs
@@ -18,6 +18,7 @@ namespace Marketplace.SaaS.Accelerator.AdminSite.Controllers;
 /// </summary>
 /// <seealso cref="BaseController" />
 [ServiceFilter(typeof(KnownUserAttribute))]
+[ServiceFilter(typeof(RequestLoggerActionFilter))]
 public class ApplicationConfigController : BaseController
 {
     private readonly ILogger<ApplicationConfigController> logger;
@@ -40,44 +41,34 @@ public class ApplicationConfigController : BaseController
     }
 
     /// <summary>
-    /// Indexes this instance.
+    /// Main action for Application Config Page
     /// </summary>
     /// <returns>return All Application Config.</returns>
+    [ServiceFilter(typeof(ExceptionHandlerAttribute))]
     public IActionResult Index()
     {
-        this.logger.LogInformation("Application Config Controller / Index");
-        try
-        {
-            IEnumerable<ApplicationConfiguration> getAllAppConfigData = new List<ApplicationConfiguration>();
-            getAllAppConfigData = this.appConfigService.GetAllApplicationConfiguration();
-            return this.View(getAllAppConfigData);
-        }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            return this.View("Error", ex);
-        }
+        this.logger.LogInformation("Application Config Controller / Index (from controller)");
+
+        IEnumerable<ApplicationConfiguration> getAllAppConfigData = new List<ApplicationConfiguration>();
+        getAllAppConfigData = this.appConfigService.GetAllApplicationConfiguration();
+        return this.View(getAllAppConfigData);
     }
 
     /// <summary>
     /// Indexes an EmailTemplate instance
     /// </summary>
     /// <returns>return a list of all EmailTemplates.</returns>
+    [ServiceFilter(typeof(ExceptionHandlerAttribute))]
     public IActionResult EmailTemplates()
     {
         this.logger.LogInformation("Application Config Controller / EmailTemplates");
-        try
-        {
-            IEnumerable<EmailTemplate> getEmailTemplateData = new List<EmailTemplate>();
-            getEmailTemplateData = this.emailTemplateRepository.GetAll();
-            return this.View(getEmailTemplateData);
-        }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            return this.View("Error", new Exception("Error loading templates." + Environment.NewLine + " Please check logs for more details."));
-        }
+
+        IEnumerable<EmailTemplate> getEmailTemplateData = new List<EmailTemplate>();
+        getEmailTemplateData = this.emailTemplateRepository.GetAll();
+        return this.View(getEmailTemplateData);
     }
+
+
 
     /// <summary>
     /// Get the fields of an EmailTemplate item by status
@@ -89,17 +80,10 @@ public class ApplicationConfigController : BaseController
     public IActionResult EmailTemplateDetails(string status)
     {
         this.logger.LogInformation("ApplicationConfig Controller / EmailTemplateDetails:  Status {0}", status);
-        try
-        {
-            EmailTemplate emailTemplate = new EmailTemplate();
-            emailTemplate = this.emailTemplateRepository.GetTemplateForStatus(status);
-            return this.PartialView(emailTemplate);
-        }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            return this.View("Error", ex);
-        }
+
+        EmailTemplate emailTemplate = new EmailTemplate();
+        emailTemplate = this.emailTemplateRepository.GetTemplateForStatus(status);
+        return this.PartialView(emailTemplate);
     }
 
     /// <summary>
@@ -113,21 +97,14 @@ public class ApplicationConfigController : BaseController
     public IActionResult EmailTemplateDetails(EmailTemplate emailTemplate)
     {
         this.logger.LogInformation("ApplicationConfig Controller / EmailTemplateDetails:  EmailTemplate {0}", JsonSerializer.Serialize(emailTemplate));
-        try
-        {
-            if (emailTemplate != null)
-            {
-                this.emailTemplateRepository.SaveEmailTemplateByStatus(emailTemplate);
-            }
 
-            this.ModelState.Clear();
-            return this.RedirectToAction(nameof(this.EmailTemplateDetails), new { status = emailTemplate.Status });
-        }
-        catch (Exception ex)
+        if (emailTemplate != null)
         {
-            this.logger.LogError(ex, ex.Message);
-            return this.PartialView("Error", ex);
+            this.emailTemplateRepository.SaveEmailTemplateByStatus(emailTemplate);
         }
+
+        this.ModelState.Clear();
+        return this.RedirectToAction(nameof(this.EmailTemplateDetails), new { status = emailTemplate.Status });
     }
 
 
@@ -142,18 +119,12 @@ public class ApplicationConfigController : BaseController
     public IActionResult ApplicationConfigDetails(int Id)
     {
         this.logger.LogInformation("ApplicationConfig Controller / AppConfigDetails:  Id {0}", Id);
-        try
-        {
-            ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
-            applicationConfiguration = this.appConfigService.GetById(Id);
-            return this.PartialView(applicationConfiguration);
-        }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            return this.View("Error", ex);
-        }
+
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+        applicationConfiguration = this.appConfigService.GetById(Id);
+        return this.PartialView(applicationConfiguration);
     }
+
 
     /// <summary>
     /// Saves the app config item changes.
@@ -166,22 +137,15 @@ public class ApplicationConfigController : BaseController
     public IActionResult ApplicationConfigDetails(ApplicationConfiguration appConfig)
     {
         this.logger.LogInformation("ApplicationConfig Controller / ApplicationConfigDetails:  AppConfig {0}", JsonSerializer.Serialize(appConfig));
-        try
+        if (appConfig != null)
         {
-            if (appConfig != null)
-            {
-                this.appConfigService.SaveAppConfig(appConfig);
-            }
+            this.appConfigService.SaveAppConfig(appConfig);
+        }
 
-            this.ModelState.Clear();
-            return this.RedirectToAction(nameof(this.ApplicationConfigDetails), new { Id = appConfig.Id });
-        }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            return this.PartialView("Error", ex);
-        }
+        this.ModelState.Clear();
+        return this.RedirectToAction(nameof(this.ApplicationConfigDetails), new { Id = appConfig.Id });
     }
+
 
     /// <summary>
     /// Upload file(s) to database
@@ -189,78 +153,70 @@ public class ApplicationConfigController : BaseController
     /// <param name="files">The files</param>
     /// <returns>RedirectToAction.</returns>
     [HttpPost("FileUpload")]
+    [ServiceFilter(typeof(ExceptionHandlerAttribute))]
     public IActionResult PostUpload(List<IFormFile> files)
     {
         this.logger.LogInformation("Application Config Controller / PostUpload ");
-        try
+        if (files == null || files.Count == 0)
         {
-            if (files == null || files.Count == 0)
+            TempData["Upload"] = "No files to upload";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            if (files.Count > 2)
             {
-                TempData["Upload"] = "No files to upload";
+                TempData["Upload"] = "No more than two files can be uploaded";
                 return RedirectToAction("Index");
+            }
+            foreach (var file in files)
+            {
+                int maxLength = 1024 * 1024 * 5; //5 MB
+
+                if (file.Length > maxLength)
+                {
+                    TempData["Upload"] = "File is too large, max size of file for upload is 5 MB";
+                    return RedirectToAction("Index");
+                }
+                if (file.Length == 0)
+                {
+                    TempData["Upload"] = "File is empty";
+                    return RedirectToAction("Index");
+                }
+
+                var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+                if (fileExtension != ".png" && fileExtension != ".ico")
+                {
+                    TempData["Upload"] = "Only .png or .ico files can be uploaded";
+                    return RedirectToAction("Index");
+                }
+
+                var appConfigNames = this.appConfigService.GetAllApplicationConfiguration().Select(a => a.Name);
+
+                if (!appConfigNames.Contains("LogoFile") || !appConfigNames.Contains("FaviconFile"))
+                {
+                    TempData["Upload"] = "LogoFile or FaviconFile application config settings are missing in the database";
+                    return RedirectToAction("Index");
+                }
+
+                if (this.appConfigService.UploadFileToDatabase(file, fileExtension) == false)
+                {
+                    TempData["Upload"] = "File Upload failed!";
+                    return RedirectToAction("Index");
+                }
+            }
+
+            if (files.Count == 1)
+            {
+                TempData["Upload"] = files.FirstOrDefault().FileName + "  uploaded successfully";
             }
             else
             {
-                if (files.Count > 2)
-                {
-                    TempData["Upload"] = "No more than two files can be uploaded";
-                    return RedirectToAction("Index");
-                }       
-                foreach (var file in files)
-                {
-                    int maxLength = 1024 * 1024 * 5; //5 MB
-
-                    if (file.Length > maxLength)
-                    {
-                        TempData["Upload"] = "File is too large, max size of file for upload is 5 MB";
-                        return RedirectToAction("Index");
-                    }
-                    if (file.Length == 0)
-                    {
-                        TempData["Upload"] = "File is empty";
-                        return RedirectToAction("Index");
-                    }
-
-                    var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
-
-                    if (fileExtension != ".png" && fileExtension != ".ico")
-                    {
-                        TempData["Upload"] = "Only .png or .ico files can be uploaded";
-                        return RedirectToAction("Index");
-                    }
-
-                    var appConfigNames = this.appConfigService.GetAllApplicationConfiguration().Select(a => a.Name);
-                       
-                    if (!appConfigNames.Contains("LogoFile") || !appConfigNames.Contains("FaviconFile"))
-                    {
-                        TempData["Upload"] = "LogoFile or FaviconFile application config settings are missing in the database";
-                        return RedirectToAction("Index");
-                    }
-
-                    if (this.appConfigService.UploadFileToDatabase(file, fileExtension) == false)
-                    {
-                        TempData["Upload"] = "File Upload failed!";
-                        return RedirectToAction("Index");
-                    }
-                }
-
-                if (files.Count == 1)
-                {
-                    TempData["Upload"] = files.FirstOrDefault().FileName + "  uploaded successfully";
-                }
-                else
-                {
-                    TempData["Upload"] = files[0].FileName + " and " + files[1].FileName + " uploaded successfully";
-                }
+                TempData["Upload"] = files[0].FileName + " and " + files[1].FileName + " uploaded successfully";
             }
-            return RedirectToAction("Index");
         }
-        catch (Exception ex)
-        {
-            this.logger.LogError(ex, ex.Message);
-            TempData["Upload"] = "File Upload failed!";
-            return this.PartialView("Error", ex);
-        }
+        return RedirectToAction("Index");
     }
 
 }
