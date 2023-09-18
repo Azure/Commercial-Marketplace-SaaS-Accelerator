e739817684f7f405dc1f85a49089e6232c5c7159
Neelavarsha Duddanahalli Kumar Mahesh <neelavarshad@microsoft.com>
2023-09-18 09:26:26 -0700

    Bootstrap 5.3.0 updates

---
+++ src/CustomerSite/wwwroot/js/script.js
@@ -0,0 +1,10 @@
+﻿
+const myModal = new bootstrap.Modal('#myModal');
+
+setTimeout(() => {
+    myModal.hide();
+},1000);
+
+//document.querySelector('.btn-close').addEventListener('click', () => {
+//    myModal.hide();
+//});
