f571d11fe0158a29ef8ae21c0aa646a9cdaf6529
msalemcode <magdyshaabansalem@hotmail.com>
2024-03-11 16:50:34 -0700

    fix printout

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -71,9 +71,9 @@ if($KeyVault -eq "")
 			Write-Host "🛑  KeyVault name "  -NoNewline -ForegroundColor Red
 			Write-Host "$KeyVault"  -NoNewline -ForegroundColor Red -BackgroundColor Yellow
 			Write-Host " already exists." -ForegroundColor Red
-			Write-Host "  To Purge KeyVault please use the following doc:"
-			Write-Host "  https://learn.microsoft.com/en-us/cli/azure/keyvault?view=azure-cli-latest#az-keyvault-purge."
-			Write-Host "  You could use new KeyVault name by using parameter" -NoNewline 
+			Write-Host "   To Purge KeyVault please use the following doc:"
+			Write-Host "   https://learn.microsoft.com/en-us/cli/azure/keyvault?view=azure-cli-latest#az-keyvault-purge."
+			Write-Host "   You could use new KeyVault name by using parameter" -NoNewline 
 			Write-Host " -KeyVault"  -ForegroundColor Green
 			exit 1
 		}
