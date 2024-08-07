450b7b25645727d83754bbb6e4ac7af453c5549c
Neelavarsha Duddanahalli Kumar Mahesh <neelavarshad@microsoft.com>
2024-08-07 10:01:23 -0700

    Modified app reg checks

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -215,7 +215,8 @@ if($LogoURLico) {
 #region Create AAD App Registrations
 
 #Record the current ADApps to reduce deployment instructions at the end
-$ISLoginAppProvided = ($ADApplicationIDAdmin && $ADMTApplicationIDPortal)
+$ISLoginAppProvided = (($ADApplicationIDAdmin -ne $null -and $ADMTApplicationIDPortal -ne $null)
+)
 
 if($ISLoginAppProvided -eq $null){
 	Write-Host "🔑 Multi-Tenant App Registrations provided."
@@ -558,7 +559,7 @@ Remove-Item -Path script.sql
 #region Present Output
 
 Write-host "✅ If the intallation completed without error complete the folllowing checklist:"
-if ($ISLoginAppProvided -ne $null) {  #If provided then show the user where to add the landing page in AAD, otherwise script did this already for the user.
+if ($ISLoginAppProvided) {  #If provided then show the user where to add the landing page in AAD, otherwise script did this already for the user.
 	Write-host "   🔵 Add The following URLs to the multi-tenant Landing Page AAD App Registration in Azure Portal:"
 	Write-host "      ➡️ https://$WebAppNamePrefix-portal.azurewebsites.net"
 	Write-host "      ➡️ https://$WebAppNamePrefix-portal.azurewebsites.net/"
